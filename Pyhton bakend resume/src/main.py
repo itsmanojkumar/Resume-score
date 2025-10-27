f2483dd85e66e11ecfe8106b88de661af7100d11
Manoj <itsmanojkumar04@gmail.com>
2025-10-27 11:49:47 +0530

    done

--- Pyhton bakend resume/src/main.py
+++ Pyhton bakend resume/src/main.py
@@ -576,4 +576,6 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Render provides PORT dynamically
+    uvicorn.run(app, host="0.0.0.0", port=port)
